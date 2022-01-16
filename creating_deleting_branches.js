62aac4c97578ac855a2cb71c352cd7e343fe33f7
Nicholas Oyagh <you@example.com>
2022-01-15 19:47:19 -0800

    files added

---
+++ creating_deleting_branches.js
@@ -0,0 +1,15 @@
+let ClientName = "James Bond";
+
+if (ClientName == "James Bond") {
+  alert("Good Agent");
+}
+// } else if (ClientName == "Hugo Drax") {
+//   alert("Bad Agent");
+// } else {
+//   alert("Mission Accomplished");
+// }
+
+let ClientNumber = 007;
+let ClientAge = 51;
+let ClientId = "MI6007247";
+let ClientPhoneNumber = 9112470007;
