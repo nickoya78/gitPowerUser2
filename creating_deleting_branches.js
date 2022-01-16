efa8defd12d0975f6b3d81a3ed0084c03a8816e3
Nicholas Oyagh <you@example.com>
2022-01-15 20:48:41 -0800

    last change

--- creating_deleting_branches.js
+++ creating_deleting_branches.js
@@ -2,8 +2,11 @@ let ClientName = "James Bond";
 
 if (ClientName == "James Bond") {
   alert("Good Agent");
+} else if (ClientName == "Hugo Drax") {
+  alert("Bad Agent");
+} else {
+  alert("Mission Accomplished");
 }
-
 let ClientNumber = 007;
 let ClientAge = 51;
 let ClientId = "MI6007247";
